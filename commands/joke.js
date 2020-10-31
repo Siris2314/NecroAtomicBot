14996d22f51c554a9813f44b7a336838daeb9725
Arihant Tripathi <tarihant2001@gmail.com>
2020-10-31 04:06:34 -0400

    joke command

---
+++ commands/joke.js
@@ -0,0 +1,22 @@
+const randomPuppy = require('random-puppy');
+const Discord = require('discord.js');
+
+
+module.exports = {
+  name: "Joke/Meme",
+  description: "Gives a joke or meme",
+  async run(message,args){
+    const subReddits = ["dankmeme", "meme", "memes"]
+    const random = subReddits[Math.floor(Math.random()) * subReddits.length]
+
+    const img = await randomPuppy(random);
+
+    const embed = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setImage(img)
+      .setTitle(`Your meme....idk how funny it is. Source: r/${random}`)
+      .setURL(`https://reddit.com/r/${random}`)
+
+    message.channel.send(embed)
+  }
+}
