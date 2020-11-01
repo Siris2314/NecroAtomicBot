c71c846af76af96c641f6cfe1bb2be0789e350bc
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-11-01 03:06:33 -0500

    covid fix

--- commands/covid.js
+++ commands/covid.js
@@ -4,29 +4,28 @@ const Discord = require('discord.js')
 
 module.exports = {
 
-  name: "Covid Command",
+  name: "covid",
   description: "Tracks Cases WorldWide",
 
-  async execute(message,args){
+  execute(message,args){
     let countries = args.join(" ");
 
     const noArgs = new Discord.MessageEmbed()
       .setTitle('Missing Arguments')
       .setColor(`00xFF0000`)
       .setDescription("Missing Arguments")
-      .setTimeStamp()
 
     if(!args[0]){
       return message.channel.send(noArgs);
     }
-
+    console.log(args);
     if(args[0] === "WorldWide"){
       fetch(`https://covid19.mathdro.id/api`)
         .then(response => response.json())
         .then(data => {
-          let confirmed = data.confirmed.value.toLocaleString()
-          let recovered = data.recovered.value.toLocaleString()
-          let deaths = data.deaths.value.toLocaleString()
+          let confirmed = data.confirmed.value.toLocaleString();
+          let recovered = data.recovered.value.toLocaleString();
+          let deaths = data.deaths.value.toLocaleString();
 
           const embed = new Discord.MessageEmbed()
           .setTitle(`WorldWide COVID-19 Cases`)
