f17347e1e3911a2bb7a9d625cd679d6d96d3e904
Arihant Tripathi <tarihant2001@gmail.com>
2021-01-11 15:26:17 -0500

    Music Commands

--- commands/play.js
+++ commands/play.js
@@ -1,33 +1,11 @@
-const fs = require('fs');
-
 module.exports = {
 
-  name: 'play',
-  description: 'plays recording',
+  name:'play',
+  description:'plays music',
 
-  async execute(message,args,client){
+  async execute(message, args,client){
 
-    const voicechannel = message.member.voice.channel;
-    if(!voicechannel){
-      return message.channel.send('Please join a vc')
-    }
-
-    if(!fs.existsSync(`./recorded-${message.author.id}.pcm`)){
-      return message.channel.send('Your audio is not recorded')
-    }
-
-      const connection  = await message.member.voice.channel.join()
-
-      const stream = fs.createReadStream(`./recorded-${message.author.id}.pcm`)
-
-      const dispatcher = connection.play(stream, {
-        type: "converted",
-
-      })
-
-      dispatcher.on("finish", () => {
-        message.member.voice.channel.leave();
-        message.channel.send('Audio has finished playing')
-      })
+    const music = args.join(" ")
+    client.distube.play(message, music)
   }
 }
---
+++ commands/stop.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name:'stop',
+  description: 'stops music',
+
+  async execute(message, args, client){
+
+    if(!message.member.voice.channel){
+      return message.channel.send('Must be in a vc to use this command')
+    }
+
+    let queue = await client.distube.getQueue(message);
+
+   if(queue){
+    client.distube.stop(message)
+
+    message.channel.send('Music has stopped playing')
+  } else if(!queue){
+    return message.channel.send("No Music Is Playing")
+
+  }
+  }
+}
