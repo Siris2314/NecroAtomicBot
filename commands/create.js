e8fd30a2c87d3ccfa6d421d233039c464957de4c
Arihant Tripathi <tarihant2001@gmail.com>
2021-01-25 01:27:38 -0500

    Create fixxer

--- commands/create.js
+++ commands/create.js
@@ -1,19 +1,16 @@
-
 const schema = require('../schemas/custom-commands.js');
 
 module.exports = {
     name: 'create',
-    description: 'Creates a custom command',
-    async execute(message,args,client) {
-        if(!message.member.hasPermission('ADMINSTRATOR')) return message.channel.send('You do not have permissions to use this command');
+    description:'creates commands',
+    async execute(message, args, client) {
+        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You do not have permissions to use this command');
 
-        const name = args[0];
-        const response = args.slice(1).join(" ");
+        const name = args[0]; const response = args.slice(1).join(" ");
 
         if(!name) return message.channel.send('Please specify a command name');
         if(!response) return message.channel.send('Please specify a response');
 
-      try{
         const data = await schema.findOne({ Guild: message.guild.id, Command: name });
         if(data) return message.channel.send('This custom commands exists already!');
         const newData =  new schema({
@@ -23,9 +20,5 @@ module.exports = {
         })
         await newData.save();
         message.channel.send(`Saved **${name}** as a custom command!`);
-      } finally{
-
-        mongoose.connection.close();
-      }
     }
 }
--- commands/delete.js
+++ commands/delete.js
@@ -1,37 +1,18 @@
-const schema = require('../schemas/custom-commands.js')
-const mongoose = require('mongoose')
+const schema = require('../schemas/custom-commands');
+
 module.exports = {
+    name: 'delete',
+    description:'deletes commands',
+    async execute(message, args, client) {
+        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You do not have permissions to use this command');
 
-  name: 'delete',
-  description: 'deletes a custom command',
+        const name = args[0];
 
-  async execute(message,args,client){
+        if(!name) return message.channel.send('Please specify a command name');
 
-    if(!message.member.hasPermission('ADMINSTRATOR')){
-      return message.channel.send('Perms Denied')
+        const data = await schema.findOne({ Guild: message.guild.id, Command: name });
+        if(!data) return message.channel.send('That custom command does not exist!');
+        await schema.findOneAndDelete({ Guild: message.guild.id, Command: name });
+        message.channel.send(`Removed **${name}** from custom commands!`);
     }
-
-    const name = args[0];
-
-
-    if(!name){
-      return message.channel.send('Please specify command name')
-    }
-
-
-
-
-    const data = await schema.findOne({Guild: message.guild.id, Command: name});
-
-    if(!data){
-      return message.channel.send('This command does not exist')
-    }
-
-    await schema.findOneAndDelete({Guild: message.guild.id, Command: name})
-    message.channel.send(`Removed **${name}** from custom commands`)
-
-
-
-
-  }
 }
