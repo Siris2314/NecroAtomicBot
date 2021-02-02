ba2a25a3de17583ca18baade92f008241493222f
Arihant Tripathi <tarihant2001@gmail.com>
2021-02-02 17:14:15 -0500

    queue command

---
+++ commands/queue.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name:'skip',
+  description: 'skip music',
+
+  async execute(message, args, client){
+
+    if(!message.member.voice.channel){
+      return message.channel.send('Must be in a vc to use this command')
+    }
+
+    let queue = await client.distube.getQueue(message);
+
+   if(queue){
+    client.distube.getQueue(message)
+
+    
+  } else if(!queue){
+    return message.channel.send("No Music Is Playing")
+
+  }
+  }
+}
