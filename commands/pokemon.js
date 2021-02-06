adfb7278110ef7ac0278846f802817d8d225837d
Arihant Tripathi <tarihant2001@gmail.com>
2021-02-06 01:51:15 -0500

    pokemon command

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -1,29 +1,9 @@
-const Discord = require('discord.js')
-const { get } = require("request-promise-native")
+const fetch = require('node-fetch');
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 
-module.exports = {
-  name:"pokemon",
-  description: "Gets pokemon description",
-
-  execute(message,args){
-
-    const options = {
-      url: `https://courses.cs.washington.edu/courses/cse154/webservices/pokedex/pokedex.php?pokemon=${args.join(" ")}`,
-      json: true
-    }
-
-    message.channel.send("Fetching Information for API").then(msg => {
-      get(options).then(body => {
-        let embed = new Discord.MessageEmbed()
-         .setAuthor(body.name, `https://courses.cs.washington.edu/courses/cse154/webservices/pokedex/${body.images.typeIcon}`)
-         .setDescription(body.info.description)
-         .setThumbnail(`https://courses.cs.washington.edu/courses/cse154/webservices/pokedex/${body.images.photo}`)
-         .setColor("RANDOM")
-         .setFooter(`Weakness of this pokemon - ${body.info.weakness}`, `https://courses.cs.washington.edu/courses/cse154/webservices/pokedex/${body.images.weaknessIcon}`)
-
-     message.channel.send(embed);
-     message.delete();
-      })
-    })
-  }
+async function getPokemon(pokemon) {
+    let response = await fetch(`${BASE_URL}/${pokemon}`);
+    return await response.json();
 }
+
+module.exports = { getPokemon };
--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const schema = require('./schemas/custom-commands.js')
 const memberCount = require('./member-count.js')
 const search = require('youtube-search')
 const DisTube = require('distube')
+const { getPokemon } = require('./commands/pokemon');
 const translate = require('@k3rn31p4nic/google-translate-api')
 client.snipes = new Map();
 
@@ -128,6 +129,35 @@ client.on ('message', async message => {
   if(!message.content.startsWith(prefix) || message.author.bot){
     return;
   }
+
+  if(message.content.toLowerCase().startsWith('!pokemon')) {
+        const pokemon = message.content.toLowerCase().split(" ")[1];
+        try {
+            const pokeData = await getPokemon(pokemon);
+            const {
+                sprites,
+                stats,
+                weight,
+                name,
+                id,
+                base_experience,
+                abilities,
+                types
+            } = pokeData;
+            const embed = new MessageEmbed();
+            embed.setTitle(`${name} #${id}`)
+            embed.setThumbnail(`${sprites.front_default}`);
+            stats.forEach(stat => embed.addField(stat.stat.name, stat.base_stat, true));
+            types.forEach(type => embed.addField('Type', type.type.name, true));
+            embed.addField('Weight', weight);
+            embed.addField('Base Experience', base_experience);
+            message.channel.send(embed);
+        }
+        catch(err) {
+            console.log(err);
+            message.channel.send(`Pokemon ${pokemon} does not exist.`);
+        }
+    }
 
   if(message.content.toLowerCase() == '!search'){
     let embed = new Discord.MessageEmbed()
