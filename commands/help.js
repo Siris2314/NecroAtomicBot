c0a79c6c7ee625537096aa99ff38d17acc7613d3
Arihant Tripathi <tarihant2001@gmail.com>
2021-02-07 03:13:04 -0500

    help command help

--- commands/help.js
+++ commands/help.js
@@ -5,104 +5,41 @@ const token = process.env.token;
 const prefix = process.env.prefix;
 
 module.exports = {
-  name: "menu",
-  aliases : ['h'],
+  name: "help",
   description: "Shows all available bot commands.",
    async execute (message, args, client) {
 
+     const data = [];
+     const {commands} = message.client;
 
-    const roleColor =
-      message.guild.me.displayHexColor === "#000000"
-        ? "#ffffff"
-        : message.guild.me.displayHexColor;
+     if(!args.length){
+       data.push("Here is the list of commands");
+       data.push(`${prefix}` + commands.map(c=>c.name).join(`\n${prefix}`))
+       data.push(`\nYou can use ${prefix}help [command name to get specific info about a command]`)
+       message.channel.send(data);
 
-    if (!args[0]) {
-      let categories = [];
+       return;
+     }
 
-      readdirSync("./commands/").forEach((dir) => {
-        const commands = readdirSync(`./commands/${dir}/`).filter((file) =>
-          file.endsWith(".js")
-        );
+     const name = args[0]
+     const cmd = commands.get(name) || commands.find(c=>c.aliases && c.aliases.includes(name))
 
-        const cmds = commands.map((command) => {
-          let file = require(`../../commands/${dir}/${command}`);
 
-          if (!file.name) return "No command name.";
+     if(!cmd){
+       message.channel.send(`${cmd} is not a valid command`)
+       return;
+     }
 
-          let name = file.name.replace(".js", "");
 
-          return `\`${name}\``;
-        });
+     data.push(`Name: ${cmd.name}`);
+     if(cmd.description){
+       data.push(`Description: ${cmd.description}`)
+     }
 
-        let data = new Object();
+     message.channel.send data;
 
-        data = {
-          name: dir.toUpperCase(),
-          value: cmds.length === 0 ? "In progress." : cmds.join(" "),
-        };
 
-        categories.push(data);
-      });
 
-      const embed = new MessageEmbed()
-        .setTitle("📬 Need help? Here are all of my commands:")
-        .addFields(categories)
-        .setDescription(
-          `Use \`${prefix}help\` followed by a command name to get more additional information on a command. For example: \`${prefix}help ban\`.`
-        )
-        .setFooter(
-          `Requested by ${message.author.tag}`,
-          message.author.displayAvatarURL({ dynamic: true })
-        )
-        .setTimestamp()
-        .setColor(roleColor);
-      return message.channel.send(embed);
-    } else {
-      const command =
-        client.commands.get(args[0].toLowerCase()) ||
-        client.commands.find(
-          (c) => c.aliases && c.aliases.includes(args[0].toLowerCase())
-        );
-
-      if (!command) {
-        const embed = new MessageEmbed()
-          .setTitle(`Invalid command! Use \`${prefix}help\` for all of my commands!`)
-          .setColor("RANDOM");
-        return message.channel.send(embed);
-      }
-
-      const embed = new MessageEmbed()
-        .setTitle("Command Details:")
-        .addField("PREFIX:", `\`${prefix}\``)
-        .addField(
-          "COMMAND:",
-          command.name ? `\`${command.name}\`` : "No name for this command."
-        )
-        .addField(
-          "ALIASES:",
-          command.aliases
-            ? `\`${command.aliases.join("` `")}\``
-            : "No aliases for this command."
-        )
-        .addField(
-          "USAGE:",
-          command.usage
-            ? `\`${prefix}${command.name} ${command.usage}\``
-            : `\`${prefix}${command.name}\``
-        )
-        .addField(
-          "DESCRIPTION:",
-          command.description
-            ? command.description
-            : "No description for this command."
-        )
-        .setFooter(
-          `Requested by ${message.author.tag}`,
-          message.author.displayAvatarURL({ dynamic: true })
-        )
-        .setTimestamp()
-        .setColor(roleColor);
-      return message.channel.send(embed);
     }
-  },
+
 };
