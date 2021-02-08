9fb35a9859392f739c1475f6f533dd4d6a4a98a6
Gabriel Weinbrenner <angelgabe101@gmail.com>
2021-02-08 16:32:55 -0500

    Data for canvas assignments

--- commands/comms.js
+++ commands/comms.js
@@ -1,12 +1,31 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+require("dotenv").config();
+const canvas = process.env.canvas;
 
 module.exports = {
+    name: "comms",
+    description: "returns due dates for intro to comms",
 
-  name:'comms',
-  description:'returns due dates for intro to comms',
+    async execute(message, args) {
+        console.log("comms");
 
-  async execute(message,args,client){
+        fetch("https://rutgers.instructure.com/api/v1/courses/107517/assignments", {
+            method: "GET",
+            headers: {
+                Authorization:
+                    "Bearer 6948~wH4Yt4g1DWAogY5mMJOYVdXpqrZx3zHNJ9NsfySIrdOpMoqwL8pfCYf6ZUq1PhH1",
+                "Content-Type": "application/json",
+            },
+        }).then((s) => {
+            s.json().then((res) => {
+                href = res[0];
 
-    
-  }
-}
+                const embed = new Discord.MessageEmbed()
+                    .setTitle(href.id)
+                    .setFooter("Powered by Canvas");
+                return message.channel.send(embed);
+            });
+        });
+    },
+};
