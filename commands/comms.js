faf26e5f15a6f703eb43d55c633c52264b6f6c1d
Arihant Tripathi <tarihant2001@gmail.com>
2021-02-08 15:15:57 -0500

    canvas command

---
+++ commands/comms.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+module.exports = {
+
+  name:'comms',
+  description:'returns due dates for intro to comms',
+
+  async execute(message,args,client){
+
+    
+  }
+}
