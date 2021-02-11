d8a204aed3af351d7dea69cbeeb55e12400ecd7b
Arihant Tripathi <tarihant2001@gmail.com>
2021-02-10 23:40:17 -0500

    music fix

--- commands/play.js
+++ commands/play.js
@@ -1,11 +1,11 @@
-module.exports = {
-
-  name:'play',
-  description:'plays music',
-
-  async execute(message, args,client){
-
-    const music = args.join(" ")
-    client.distube.play(message, music)
-  }
-}
+// module.exports = {
+//
+//   name:'play',
+//   description:'plays music',
+//
+//   async execute(message, args,client){
+//
+//     const music = args.join(" ")
+//     client.distube.play(message, music)
+//   }
+// }
