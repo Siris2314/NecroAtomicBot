c082c319b6e743c5c28a6f9a41d380b7a1a73a5f
Arihant Tripathi <tarihant2001@gmail.com>
2021-02-13 11:26:20 -0500

    loop fixxes

--- commands/loop.js
+++ commands/loop.js
@@ -13,7 +13,7 @@ module.exports = {
 
     if(0 <= Number(args[0]) && Number(args[0]) <= 2){
                 distube.setRepeatMode(message,parseInt(args[0]))
-                embedbuilder(client, message, "GREEN", "Repeat mode set to:!", `${args[0].replace("0", "OFF").replace("1", "Repeat song").replace("2", "Repeat Queue")}`)
+                embedbuilder(client, message, "GREEN", "Repeat mode set")
             }
             else{
                 embedbuilder(client, message, "RED", "ERROR", `Please use a number between **0** and **2**   |   *(0: disabled, 1: Repeat a song, 2: Repeat all the queue)*`)
