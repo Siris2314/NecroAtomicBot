d2983c70dd121d9262b3e3d524ec0909a1716ecb
Arihant Tripathi <tarihant2001@gmail.com>
2021-02-13 00:06:30 -0500

    more channels

--- commands/channelCreate.js
+++ commands/channelCreate.js
@@ -23,7 +23,7 @@ module.exports = {
       return message.reply('Please specify a channel name')
     }
 
-    message.guilds.channels.create(channelNameQuery).then((ch) => {
+  client.message.guilds.channels.create(channelNameQuery).then((ch) => {
         message.channel.send(`Click ${ch} to access the newly created channel`)
     });
   },
