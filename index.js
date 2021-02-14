cf96bc237949335581d463af47d2d2ed60bca27d
Arihant Tripathi <tarihant2001@gmail.com>
2021-02-13 22:29:16 -0500

    queue removal

--- index.js
+++ index.js
@@ -225,12 +225,6 @@ client.on ('message', async message => {
   if(data){
     message.channel.send(data.Response)
   }
-  if (message.content.toLowerCase() == '!necroqueue') {
-      let queue = distube.getQueue(message);
-      message.channel.send('Current queue:\n' + queue.songs.map((song, id) =>
-          `**${id+1}**. [${song.name}](${song.url}) - \`${song.formattedDuration}\``
-      ).join("\n"));
-  }
 
 
 
