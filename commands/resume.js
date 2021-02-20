60b5c7f79b107022760c14011d2ba55b00309341
Arihant Tripathi <tarihant2001@gmail.com>
2021-02-20 12:51:16 -0500

    resume fix

--- commands/resume.js
+++ commands/resume.js
@@ -3,7 +3,7 @@ module.exports = {
     description:'resumes paused music',
     aliases: ["resume", "unpause"],
     inVoiceChannel: true,
-    async execute(message, args,client) => {
+    async execute(message, args,client){
         const queue = client.distube.getQueue(message)
         if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing in the queue right now!`)
         client.distube.resume(message)
