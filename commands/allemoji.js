97e18a5ef932562fe4ace3a048f4ae51e37f052e
Arihant Tripathi <tarihant2001@gmail.com>
2021-02-21 23:22:09 -0500

    more emoji fix

--- commands/allemoji.js
+++ commands/allemoji.js
@@ -1,36 +1,33 @@
-const Discord = require('discord.js')
 
+const { MessageEmbed } = require("discord.js");
 module.exports = {
-
-  name:'allemoji',
-  description:'Views all emojis in the guild',
-
-  async execute(message, args, client){
-    let emojis = "";
-    let emojisanimated = "";
-    let emojicount = 0;
-    let animated = 0;
-    let overallemojis = 0;
-
-    function emoji(id){
-      return client.emojis.cache.get(id).toString()
+  name: "emoji",
+  description: "View all emojis in the guild",
+  async execute(message, args,client){
+    let Emojis = "";
+    let EmojisAnimated = "";
+    let EmojiCount = 0;
+    let Animated = 0;
+    let OverallEmojis = 0;
+    function Emoji(id) {
+      return client.emojis.cache.get(id).toString();
     }
-    message.guild.emojis.cache.forEach(emoji => {
-      overallemojis++;
-      if(emoji.animated){
-        animated++;
-        emojisanimated+=emoji(emoji.id)
+    message.guild.emojis.cache.forEach((emoji) => {
+      OverallEmojis++;
+      if (emoji.animated) {
+        Animated++;
+        EmojisAnimated += Emoji(emoji.id);
+      } else {
+        EmojiCount++;
+        Emojis += Emoji(emoji.id);
       }
-      else{
-        emojicount++;
-        emojis+=emoji(emoji.id)
-      }
-    })
-    let embed = new Discord.MessageEmbed()
+    });
+    let Embed = new MessageEmbed()
       .setTitle(`Emojis in ${message.guild.name}.`)
-      .setDescription(`**Animated [${Animated}]**:\n${emojisanimated}\n\n**Standard**[${emojicount}]**:\n${emojis}\n\n**Overall Emojis [${overallemojis}]**`)
-      .setColor('RANDOM')
-
-    message.channel.send(embed)
-  }
-}
+      .setDescription(
+        `**Animated [${Animated}]**:\n${EmojisAnimated}\n\n**Standard [${EmojiCount}]**:\n${Emojis}\n\n**Over all emojis [${OverallEmojis}]**`
+      )
+      .setColor(`RANDOM`);
+    message.channel.send(Embed);
+  },
+};
