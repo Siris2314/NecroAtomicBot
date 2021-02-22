71741a5e9eb4277d11a77d05079a504f67da3cd2
Arihant Tripathi <tarihant2001@gmail.com>
2021-02-22 14:09:50 -0500

    music bot player fix

--- index.js
+++ index.js
@@ -33,20 +33,25 @@ const opts = {
 client.distube = new DisTube(client, { searchSongs: false, emitNewSongOnly: false, youtubeCookie: key1});
 const status = queue => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode === 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``
 client.distube
-    .on("playSong", (message, queue, song) => message.channel.send(
-        message.channel.send(`${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n${status(queue)}`)
-    ))
-    .on("addSong", (message, queue, song) => message.channel.send(
-        message.channel.send(`${client.emotes.success} | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`)
-    ))
-    .on("playList", (message, queue, playlist, song) => message.channel.send(
-        message.channel.send(`${client.emotes.play} | Play \`${playlist.title}\` playlist (${playlist.total_items} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`)
-    ))
-    .on("addList", (message, queue, playlist) => message.channel.send(
-        message.channel.send(`${client.emotes.success} | Added \`${playlist.title}\` playlist (${playlist.total_items} songs) to queue\n${status(queue)}`)
-    ))
-    .on("empty", message => message.channel.send("Channel is empty. Leaving the channel"))
-    .on("error", (message, err) => message.channel.send(`${client.emotes.error} | An error encountered: ${err}`))
+  .on("playSong", (message, queue, song) => message.channel.send(
+    `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n${status(queue)}`
+))
+  .on("addSong", (message, queue, song) => message.channel.send(
+    `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+))
+  .on("playList", (message, queue, playlist, song) => message.channel.send(
+    `Play \`${playlist.name}\` playlist (${playlist.songs.length} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
+))
+  .on("addList", (message, queue, playlist) => message.channel.send(
+    `Added \`${playlist.name}\` playlist (${playlist.songs.length} songs) to queue\n${status(queue)}`
+))
+// DisTubeOptions.searchSongs = true
+  .on("searchResult", (message, result) => {
+    let i = 0;
+    message.channel.send(`**Choose an option from below**\n${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`);
+})
+// DisTubeOptions.searchSongs = true
+  .on("searchCancel", (message) => message.channel.send(`Searching canceled`))
 
 client.commands = new Discord.Collection();
 client.giveawaysManager = new GiveawaysManager(client, {
