13d9a499dbc0d2e4d2936bd0642aa8eedbb1d5d4
Arihant Tripathi <tarihant2001@gmail.com>
2021-02-22 21:24:39 -0500

    google images

---
+++ commands/imagesearch.js
@@ -0,0 +1,19 @@
+const img = require('images-scraper')
+
+const google = new img({
+    puppeteer : {
+        headless : true,
+    }
+})
+
+module.exports = {
+    name : 'imagesearch',
+    description:'Searches Images on Google',
+    async execute(message, args,client){
+        const query = args.join(" ")
+        if(!query) return message.channel.send('Please enter a search query')
+
+        const results = await google.scrape(query, 1)
+        message.channel.send(results[0].url);
+    }
+}
