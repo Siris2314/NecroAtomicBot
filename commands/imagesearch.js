d9ce8d74d1fea5fa75d4a2692452ea9e209e245f
Arihant Tripathi <tarihant2001@gmail.com>
2021-02-24 01:33:52 -0500

    more changes

--- commands/imagesearch.js
+++ commands/imagesearch.js
@@ -1,11 +1,12 @@
 const img = require('images-scraper')
 
-const google = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 
 module.exports = {
     name : 'imagesgoogle',
     description:'Searches Images on Google',
     async execute(message, args,client){
+
+        const google = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
         const query = args.join(" ")
         if(!query) return message.channel.send('Please enter a search query')
 
