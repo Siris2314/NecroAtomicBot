05c4794d05656af91cf7eee841e93232859ad193
Arihant Tripathi <tarihant2001@gmail.com>
2021-02-28 20:00:20 -0500

    auto voice channel maker

--- index.js
+++ index.js
@@ -309,6 +309,26 @@ client.translate = async(text, message) => {
   return translated.text;
 }
 
+const voiceCollection = new Discord.Collection()
+
+client.on("voiceStateUpdate", async (oldState, newState) => {
+  const user = await client.users.fetch(newState.id)
+  const member = newState.guild.member(user)
+
+  if(!oldState.channel && newState.channel.id === "815461483219517450"){
+    const channel = await newState.guild.channels.create(user.tag, {
+      type: "voice",
+      parent: newState.channel.parent,
+    });
+    member.voice.setChannel(channel);
+    voiceCollection.set(user.id, channel.id)
+  } else if(!newState.channel){
+    if(oldState.channelID === voiceCollection.get(newState.id)){
+      return oldState.channel.delete();
+    }
+  }
+});
+
 
 
 
@@ -351,6 +371,8 @@ client.on('guildMemberAdd', async (member) => {
             console.log(error)
         }
 })
+
+
 
 
 
