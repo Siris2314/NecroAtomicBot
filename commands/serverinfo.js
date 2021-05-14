64efbd2de58fcd1dc3445bebc8c310e3b5cb9766
Arihant Tripathi <tarihant2001@gmail.com>
2021-05-14 05:48:51 -0400

    Advanced Server and User info Commands and Spotify Playlist Fix

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,43 +1,73 @@
-const {Client, Message, MessageEmbed} = require('discord.js')
-const moment = require('moment')
-
+const Discord = require("discord.js");
+ 
 module.exports = {
-
-  name:"serverinfo",
-  description: "returns info of server",
-
-  async execute(message,args,client){
-    const guild = message.guild;
-    const embed = new MessageEmbed()
-      .setTitle(message.guild.name)
-      .setThumbnail(message.guild.iconURL())
-      .setColor('RANDOM')
-      .addField('General Info', [
-        `ID: ${guild.id}`,
-        `Name: ${guild.name}`,
-        `Owner: ${guild.owner}`
-      ])
-      .addField('Counts',[
-        `Role: ${guild.roles.cache.size} roles`,
-        `Channels: ${guild.channels.cache.size} total 
-            (Text:${guild.channels.cache.filter(
-              (ch)=>ch.type === "text").size
-            }, Voice:${guild.channels.cache.filter(
-              (ch)=>ch.type === "voice").size
-            })`, 
-        `Emojis: ${guild.emojis.cache.size} (Regular: ${guild.emojis.cache.filter((e) => !e.animated).size}, Animated: ${guild.emojis.cache.filter((e) => e.animated).size})`
-      ])
-      .addField("Miscellaneous Information", [
-        `Created:${moment(guild.createdTimestamp).format('LT')}, ${moment(guild.createdTimestamp).format('LL')}, ${moment(guild.createdTimestamp).fromNow()}`, 
-        `Region: ${guild.region}`, 
-        `Boost Tier: ${guild.premiumTier ? `Tier ${guild.premiumTier}` : "None" }`, 
-        `Boost Count: ${guild.premiumSubscriptionCount || "0"}`,
-      ])
-
-    message.channel.send(embed);
+  name: 'serverinfo',
+  description:'Shows Statistics about server',
+ 
+  async execute(message, args,client) {
+    if (!message.guild) return message.channel.send("this command is only meant to be used in servers.")
+        function checkDays(date) {
+        let now = new Date();
+        let diff = now.getTime() - date.getTime();
+        let days = Math.floor(diff / 86400000);
+        return days + (days == 1 ? " day" : " days") + " ago";
+    };
+ 
+     
     
+    
+ 
+    const channels = message.guild.channels.cache.array();
+    const textChannels = channels.filter(c => c.type === 'text').length;
+    const voiceChannels = channels.filter(c => c.type === 'voice').length;
+    const categoryChannels = channels.filter(c => c.type === 'category').length
+ 
+ 
+  const region = {
+    'us-central': `US Central`,
+    'us-east': `US East`,
+    'us-south': `US South`,
+    'us-west': `US West`,
+    'europe': `Europe`,
+    'singapore': `Singapore`,
+    'japan': `Japan`,
+    'russia': `Russia`,
+    'hongkong': `Hong Kong`,
+    'brazil': `Brazil`,
+    'sydney': `Sydney`,
+    'southafrica': `South Africa`,
+    'india': `India`,
+    'europe': `Europe`
+};
+ 
+const verificationLevels = {
+    NONE: 'None',
+    LOW: 'Low',
+    MEDIUM: 'Medium',
+    HIGH: 'High',
+    VERY_HIGH: 'Very High'
+};
+ 
+ 
+    const embed = new Discord.MessageEmbed()
+        .setAuthor(message.guild.name, message.guild.iconURL)
+        .setThumbnail(message.guild.iconURL({ dynamic: true }))
+        .addField("Name", message.guild.name, true)
+        .addField("ID", message.guild.id, true)
+        .addField('Verification Level', verificationLevels[message.guild.verificationLevel], true)
+        .addField("Members", `${message.guild.memberCount}`, true)
+        .addField('Highest Role', message.guild.roles.highest, true)
+        .addField('Emoji Count', `${message.guild.emojis.cache.size} Emojis`, true)
+        .addField('Region', region[message.guild.region], true)
+        .addField('Partnered', `${message.guild.partnered}`, true)
+        .addField('Verified', `${message.guild.verified}`, true)
+        .addField('Boosts', message.guild.premiumSubscriptionCount >= 1 ? `${message.guild.premiumSubscriptionCount}` : `0`, true)
+        .addField("Channels", `${message.guild.channels.cache.size} Total\n${textChannels} Text\n${voiceChannels} Voice\n${categoryChannels} Total Categories`, true)
+        .addField("Roles", message.guild.roles.cache.size, true)
+        .addField("Creation Date", `${message.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.channel.guild.createdAt)})`, true)
+        .setColor("#d4c5a2")
+        .setThumbnail(message.guild.iconURL({ dynamic: true }))
+    return message.channel.send(embed);
+ 
   }
-
-
-  
 }
--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,43 +1,63 @@
+const Discord = require('discord.js') 
+const moment = require('moment')
 module.exports = {
       name: 'userinfo',
       description: 'Displays info for a user',
-      execute(message, args){
-          // console.log(message.author);
+      async execute(message, args,client){
+        
+        const flags = {
+            DISCORD_EMPLOYEE: 'Discord Employee',
+            DISCORD_PARTNER: 'Discord Partner',
+            BUGHUNTER_LEVEL_1: 'Bug Hunter (Level 1)',
+            BUGHUNTER_LEVEL_2: 'Bug Hunter (Level 2)',
+            HYPESQUAD_EVENTS:'HypeSquad Events',
+            HOUSE_BRAVERY: 'House of Bravery',
+            HOUSE_BALANCE: 'House of Balance',
+            EARLY_SUPPORTER: 'Early Supporter',
+            TEAM_USER: 'Team User',
+            SYSTEM: 'System',
+            VERIFIED_BOT: 'Verified Bot',
+            VERIFIED_DEVELOPER: 'Verified Bot Developer'
+        };
 
-          const user = message.author;
-          const member = message.guild.member(user);
-          // console.log(member);
-
-          const m = {
-              title: "User Info",
-              color: 3973927,
-              image: {
-                  url: user.avatarURL(),
-              },
-              fields: [
-                  {
-                      name: "User tag",
-                      value: `${user.username}#${user.discriminator}`,
-                  },
-                  {
-                      name: "Is bot",
-                      value: user.bot,
-                  },
-                  {
-                      name: "Joined Discord",
-                      value: new Date(user.createdAt),
-                  },
-                  {
-                      name: "Joined Server",
-                      value: new Date(member.joinedTimestamp),
-                  },
-                  {
-                      name: "Roles",
-                      value: member._roles.length,
-                  },
-              ],
-              timestamp: new Date(),
-          };
-          message.channel.send({ embed: m });
+        function trimArray(arr, maxLen = 10){
+            if(arr.length > maxLen){
+                const len = arr.length - maxLen;
+                arr = arr.slice(0, maxLen);
+                arr.push(`${len} more...`)
+            }
+            return arr;
         }
+
+        const member = message.mentions.members.first() || message.member;
+        const roles = member.roles.cache 
+            .sort((a,b) => b.position - a.position)
+            .map(role => role.toString())
+            .slice(0,-1)
+
+        const userFlags = member.user.flags.toArray();
+        const embeduserinfo = new Discord.MessageEmbed()
+            .setThumbnail(member.user.displayAvatarURL({dynamic: true, size:512}))
+            .setAuthor("Information about: " + member.user.username + "#" + member.user.discriminator, member.user.displayAvatarURL({dynamic: true}))
+            .setColor("RANDOM")
+            .addField('**Username:**', `\`${member.user.username}#${member.user.discriminator}\``,true)
+            .addField('**ID:**',`\`${member.id}\``,true)
+            .addField('**Avatar:**',`[\`Link to avatar\`](${member.user.displayAvatarURL({dynamic: true})})`,true)
+            .addField('**Registered Date:**',`\`${moment(member.user.createdTimestamp).format('LT')} ${moment(member.user.createdTimestamp).format('LL')} ${moment(member.user.createdTimestamp).fromNow()} \``, true)
+            .addField('**Date Joined Server:**',`\`${moment(member.joinedAt).format('LL LTS')}\``,true)
+            .addField('**Flags:**',`\`${userFlags.length ? userFlags.map(flag => flags[flag]).join(', ') : 'None'}\``, true)
+            .addField('**Status:**',`\`${member.user.presence.status}\``,true)
+            .addField('**Game**:',`\`${member.user.presence.game || 'Not Currently Playing a Game.'}\``,true)
+            .addField('**Highest Role:**',`${member.roles.highest.id === message.guild.id ? 'None' : member.roles.highest}`,true)
+            .addField(`\`${roles.length}\` **Roles:**`,`${roles.length < 10 ? roles.join(', '): roles.length > 10 ? trimArray(roles): 'None'}`)
+            .setFooter(message.author.username, message.author.displayAvatarURL({dynamic: true}))
+            .setTimestamp()
+
+
+        return message.channel.send(embeduserinfo);
+
+
+
+        
+    }
 }
