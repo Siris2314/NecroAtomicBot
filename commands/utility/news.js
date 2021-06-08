7ec39f267c50ab95d24a4002b0018e3e1637346c
gabrielweinbrenner <gabriel.weinbrenner@gmail.com>
2021-06-08 01:41:22 -0400

    countries codes

--- commands/utility/news.js
+++ commands/utility/news.js
@@ -2,59 +2,30 @@ const { MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
 require("dotenv").config();
 const news = process.env.news;
+const countries = require("./country.json");
 
 module.exports = {
   name: "news",
   description: "Shows top 5 news headlines",
   async execute(message, args,client) {
     try {
-        const country = args.join(" ");
+        let country = args.join(" ");
+
+        if(country.length != 2){
+            country = countries[country];    
+        }
         if(!country){
             const optionembed = new MessageEmbed()
              .setTitle('Country Options')
              .addField(
-                 'Argentina - ar\n',
-                 'Australia - au\n',
-                 'Austria - at\n',
-                 'Belguim - be\n',
-                 'Brazil - br\n',
-                 'Bulgaria - bg\n',
-                 'Canada - ca\n',
-                 'China - cn\n',
-                 'Colombia - co\n',
-                 'Cuba - cu\n',
-                 'Czech Republic - cz\n',
-                 'Egypt - eg\n',
-                 'France - fr\n',
-                 'Germany - de\n',
-                 'Greece - gr\n',
-                 'Hong Kong - hk\n',
-                 'Hungary - hu\n',
-                 'India - in\n',
-                 'Indonesia - id\n',
-                 'Ireland - ie\n',
-                 'Isreal - il\n',
-                 'Italy - it\n',
-                 'Japan - jp\n',
-                 'Latvia - lv\n',
-                 'Lituania - lt\n',
-                 'Malaysia - my\n',
-                 'Mexico - mx\n',
-                 'Morocco - ma\n',
-                 'Netherlands - nl\n',
-                 'New Zeland - nz'
+                 "Put correct country (ex: China or CN)"
              )
              .setColor('RANDOM')
              .setTimestamp()
 
 
             return message.channel.send(optionembed);
-
-
                   
-
-                  
-             
         }
       const response = await fetch(
         `https://newsapi.org/v2/top-headlines?country=${country}&pageSize=5&apiKey=${news}`
