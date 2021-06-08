787ec3eadf95565d7ec1803538a4025fe7fe06a3
Arihant Tripathi <tarihant2001@gmail.com>
2021-06-08 04:48:31 -0400

    Moderation and Utility Fixes

--- commands/administration/nuke.js
+++ commands/administration/nuke.js
@@ -1,26 +1,42 @@
-const {Message} = require('discord.js')
-
 module.exports = {
+  name:'nuke',
+  description:'nukes a channel',
+  async execute(message, args,client) {
+      if(!message.member.hasPermission('MANAGE_CHANNELS') || !message.member.hasPermission('ADMINISTRATOR')) {
+          message.reply(`You can\'t use this command you need \`MANAGE_CHANNELS\` permission.`)
+          return;
+      }
+      
+      if(!message.guild.me.hasPermission('MANAGE_CHANNELS') || !message.guild.me.hasPermission('ADMINISTRATOR')) {
+          message.reply('I need \`MANAGE_CHANNELS\` permission to run this command.')
+          return
+      }
 
-  name: 'nuke',
-  description: 'nukes channels',
 
-  async execute(message, args, client) {
+      const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]) || message.channel;
+  
+      let position = channel.position
+      try {
+      
+          await channel.clone().then((ch) => {
+             
+              ch.setPosition(position)
+             
+              channel.delete().catch(err => {
 
-    if(!message.member.hasPermission('MANAGE_CHANNELS')){
-      return message.channel.send('Cannot use this command')
-    }
+                  ch.delete()
+                 
+                  message.channel.send(`I can't nuke the channel due error: \`Undefined\``)
+                  return
+              })
 
-    if(!message.guild.me.hasPermission('MANAGE_CHANNELS')){
-      return message.channel.send('I need manage channels perm')
-    }
+          
+  
+              ch.send("https://i.pinimg.com/originals/06/c3/92/06c392b847166a9a671bfcd590d8fff7.gif")
+              })
 
-    message.channel.clone().then((ch) => {
-      ch.setParent(message.channel.parent.id)
-      ch.setPosition(message.channel.position)
-      message.channel.delete();
-
-      ch.send('This channel has been nuked')
-    })
+      } catch(err) {
+          message.channel.send(`I can't nuke the channel due error: \`Undefined\``)
+      }
   }
 }
