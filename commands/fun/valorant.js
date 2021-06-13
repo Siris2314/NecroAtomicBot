f977640ef02d6141a74dfcba733c2f272b20f865
Arihant Tripathi <tarihant2001@gmail.com>
2021-06-12 22:14:46 -0400

    Valorant Command(Beta)

--- commands/fun/valorant.js
+++ commands/fun/valorant.js
@@ -20,9 +20,19 @@ module.exports = {
 
         const allAgents = await valclient.getAgents(agent)
 
+        const allWeapons = await valclient.getWeapons();
+
         const embed = new Discord.MessageEmbed()
            .setTitle(allAgents.data.displayName)
-           
+           .addField(`Description: `,`${allAgents.data.description}`,true)
+           .addField(`Role:`,`${allAgents.data.role.displayName}`,false)
+           .addField('Role Description: ',`${allAgents.data.role.description}`,false)
+           .addField('Developer Name: ', `${allAgents.data.developerName}`,false)
+           .addField("Ability 1: ", `${allAgents.data.abilities[0].displayName} - ${allAgents.data.abilities[0].description}`, false)
+           .setThumbnail(allAgents.data.displayIcon)
+
+        // console.log(allWeapons);
+        
         message.channel.send(embed)
     }
 }
