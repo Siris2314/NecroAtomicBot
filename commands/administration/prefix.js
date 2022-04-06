c32e557ab978bbb69d1d36e036e85ff13f6277b0
Arihant Tripathi <tarihant2001@gmail.com>
2022-04-06 17:41:50 -0400

    Fixed Entire Prefix System

--- commands/administration/prefix.js
+++ commands/administration/prefix.js
@@ -9,11 +9,11 @@ module.exports = {
         message.delete();
 
         if (!message.member.permissions.has('MANAGE_GUILD')) {
-            return message.channel.send({content:'You do not have permission to use this command!'}).then(m => m.delete({timeout: 10000}));
+            return message.channel.send({content:'You do not have permission to use this command!'})
         };
 
         if(!message.guild.me.permissions.has('MANAGE_GUILD')) {
-            return message.channel.send({content:'I do not have enough perms to use this command!'}).then(m => m.delete({timeout: 10000}));
+            return message.channel.send({content:'I do not have enough perms to use this command!'})
         }
 
         const settings = await Guild.findOne({
@@ -32,12 +32,12 @@ module.exports = {
                 .then(result => console.log(result))
                 .catch(err => console.error(err));
 
-                return message.channel.send({content:'This server was not in our database! We have added it, please retype this command.'}).then(m => m.delete({timeout: 10000}));
+                return message.channel.send({content:'This server was not in our database! We have added it, please retype this command, your current prefix is !necro'}).then(m => m.delete({timeout: 100000}));
             }
         });
 
         if (args.length < 1) {
-            return message.channel.send({content:`You must specify a prefix to set for this server! Your current server prefix is \`${settings.prefix}\``}).then(m => m.delete({timeout: 10000}));
+            return message.channel.send({content:`You must specify a prefix to set for this server! Your current server prefix is \`${settings.prefix}\``}).then(m => m.delete({timeout: 100000}));
         };
 
         await settings.updateOne({
