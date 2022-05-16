27dba700dcfe56309b5cb65eb4a152a332624aac
Arihant Tripathi <tarihant2001@gmail.com>
2022-05-16 03:11:20 -0400

    Heroku testing

--- index.js
+++ index.js
@@ -491,22 +491,22 @@ Canvas.loadImage("./assets/background.jpg").then(async (img) => { //Read the Bac
 
 
 // // Anti-Crash System that I use from time to time
-// process.on("unhandledRejection", (reason, p) => {
-//     console.log(" [antiCrash] :: Unhandled Rejection/Catch");
-//     // console.log(reason, p);
-// });
-// process.on("uncaughtException", (err, origin) => {
-//     console.log(" [antiCrash] :: Uncaught Exception/Catch");
-//     // console.log(err, origin);
-// });
-// process.on("uncaughtExceptionMonitor", (err, origin) => {
-//     console.log(" [antiCrash] :: Uncaught Exception/Catch (MONITOR)");
-//     // console.log(err, origin);
-// });
-// process.on("multipleResolves", (type, promise, reason) => {
-//     console.log(" [antiCrash] :: Multiple Resolves");
-//     // console.log(type, promise, reason);
-// });
+process.on("unhandledRejection", (reason, p) => {
+    console.log(" [antiCrash] :: Unhandled Rejection/Catch");
+    // console.log(reason, p);
+});
+process.on("uncaughtException", (err, origin) => {
+    console.log(" [antiCrash] :: Uncaught Exception/Catch");
+    // console.log(err, origin);
+});
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+    console.log(" [antiCrash] :: Uncaught Exception/Catch (MONITOR)");
+    // console.log(err, origin);
+});
+process.on("multipleResolves", (type, promise, reason) => {
+    console.log(" [antiCrash] :: Multiple Resolves");
+    // console.log(type, promise, reason);
+});
 
 
 //Message Event
