4e2e5ffd2e5b53e8198cf829d2b3545ce188afa8
hypwreck <89440730+Hypwreck@users.noreply.github.com>
2023-09-23 10:12:45 +0500

    Update flip.js

--- slashcmd/fun/flip.js
+++ slashcmd/fun/flip.js
@@ -13,7 +13,7 @@ module.exports = {
           'Tails'
           ];
           const embed = new MessageEmbed()
-          .setColor(client.config.color)
+          .setColor("RANDOM)
           .setDescription(`It's **${ht}**`)
         interaction.reply({embeds: [embed]})
     }
