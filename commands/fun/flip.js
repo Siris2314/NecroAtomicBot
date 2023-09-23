169a8fb14224fe22c84975f30ccf70e31a1d6e79
hypwreck <89440730+Hypwreck@users.noreply.github.com>
2023-09-23 10:11:19 +0500

    Create flip.js
    Flip Command (Message Command)

---
+++ commands/fun/flip.js
@@ -0,0 +1,16 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+    name: 'flip',
+    description: 'Flips A Coin',
+    category: "fun",
+
+    async execute(message, args, client){
+            let ht = ['Heads',
+          'Tails'
+          ];
+          const embed = new MessageEmbed()
+          .setColor(client.config.color)
+          .setDescription(`It's **${ht}**`)
+       message.channel.send({embeds: [embed]})
+    }
+  }
