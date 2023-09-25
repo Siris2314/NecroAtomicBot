83473b1208277d63026897371d7bc9a41725c7e2
hypwreck <89440730+Hypwreck@users.noreply.github.com>
2023-09-25 14:23:35 +0500

    Create bslap.js
    An Image in which Batman Slaps Robin, and the reason of the slap and the response of the slap is customizable.

---
+++ slashcmd/fun/bslap.js
@@ -0,0 +1,45 @@
+const { Client, CommandInteraction } = require("discord.js");
+const Discord = require('discord.js');
+module.exports = {
+    name: 'bslap',
+    description: 'Batman Slaps Robin.',
+    options: [
+    {
+        name: "user",
+        description: "mention that user",
+        type: "USER",
+        required: true
+      },
+      {
+        name: "batman",
+        description: "Need a reason to slap",
+        type: "STRING",
+        required: true
+      },
+      {
+        name: "robin",
+        description: "Reaction to slap. i.e: Bruh",
+        type: "STRING",
+        required: true
+      }
+],
+    /**
+     * 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     */
+  run: async (client, interaction) => {
+const user = interaction.options.getUser("user");
+const robin = interaction.options.getString("robin");
+const batman = interaction.options.getString("batman");
+let member;
+if(user) member = interaction.guild.members.cache.get(user.id);
+const Embed = new Discord.MessageEmbed()
+.setColor(client.config.color)
+.setImage(encodeURI(`https://vacefron.nl/api/batmanslap?text1=${robin}&text2=${batman}&batman=${interaction.user.avatarURL({ format: "png" })}&robin=${member.user.displayAvatarURL({ format: "png" })}`))
+.setFooter(`Requested by ${interaction.user.username}`)
+.setTimestamp()
+
+return interaction.followUp({embeds: [Embed]});
+    }
+}
